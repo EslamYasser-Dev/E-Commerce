dc92888341591db5e7c7663f86ca1700a9419bb6
eslamyasser <e.yasser997@gmail.com>
2023-06-19 18:02:18 +0300

    complete the clone

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        container:"1440px"
+      },
+      screens:{
+        xs:"320px",
+        sm:"375px",
+        sml:"500px",
+        md:"667px",
+        mdl:"768px",
+        lg:"960px",
+        lgl:"1024px",
+        xl:"1280px"
+      },
+      fontFamily:{
+        titleFont:"Roboto",
+        bodyFont:"Poppins"
+      },
+      colors:{
+        amazon_blue:"#131921",
+        amazon_light:"#232F3E",
+        amazon_yellow:"#FEBD69",
+        whiteText:"#FFFFFF",
+        lightText:"#CCC",
+        quantity_box:"#F0F2F2",
+        footerBottom:"#131A22",
+      },
+      boxShadow:{
+      }
+    },
+    plugins: []
+  }
+}
