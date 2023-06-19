5fce06352610031c97a35892a942e4b71d43694a
eslamyasser <e.yasser997@gmail.com>
2023-06-19 18:12:36 +0300

    playing with header

---
+++ src/components/header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full bg-amazon_blue text-white px-4 py-3'>
+        {}
+        {}
+        {}
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,8 @@ module.exports = {
         footerBottom:"#131A22",
       },
       boxShadow:{
+        testShadow:"0px 0px 32px 1px rgba(199,199,199,1)",
+        amazonInput:"0 0 3px 2px rgb(228 121 17 / 50%)",
       }
     },
     plugins: []
