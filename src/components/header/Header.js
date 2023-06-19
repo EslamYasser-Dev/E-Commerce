c7cb9149d197448c2dbd9985ca999200e1e8c385
eslamyasser <e.yasser997@gmail.com>
2023-06-19 23:36:50 +0300

    header hover applys

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,38 +1,29 @@
 import React from 'react'
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 import { logo } from "../../assets/index"
 
 const Header = () => {
   return (
     <div>
       <div className='w-full bg-amazon_blue text-white px-4 py-3 flex items-center gap-4'>
-        <div className="px-2 h-[80%] flex items-center border border-transparent
-         hover:border-white cursor-pointer duration-100">
+        <div className="headerhover">
           <img className="w-24 mt-2" src={logo} alt="logo" />
         </div>
-        <div className="px-2 h-[80%] flex items-center border border-transparent
-         hover:border-white cursor-pointer duration-100">
-        <LocationOnOutlinedIcon />
-        <p className='text-sm '>Deliver to <span>Egypt</span></p>
+
+        <div className="headerhover">
+          <LocationOnOutlinedIcon />
+          <p className="text-sm text text-lightText font-light flex flex-col">Deliver to
+            <span className="text-sm font-semibold -mt-1 text-whiteText">Egypt</span></p>
+        </div>
+
+        <div>
+          <span>All <span></span><ArrowDropDownOutlinedIcon /></span>
+        </div>
+
+
+
       </div>
-      </div>
-
-    
-
-
-      { }
-      { } 
-      { }
-
-
-
-
-
-
-
-
-
-
     </div>
   )
 }
