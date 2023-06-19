9657edc16a37532d7fce96558f456ebe8ce08e9f
eslamyasser <e.yasser997@gmail.com>
2023-06-20 00:44:23 +0300

    all drop DropDown List Items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,89 @@ import { logo } from "../../assets/index"
 
 const Header = () => {
   const [showAll, setShowAll] = useState(false);
-  console.log(showAll)
+  const allItems = [
+    {
+      _id: 101, title: "All Departments"
+    },
+    {
+      _id: 102, title: "Automotive"
+    },
+    {
+      _id: 103, title: "Baby"
+    },
+    {
+      _id: 104, title: "Beauty & personal care"
+    },
+    {
+      _id: 105, title: "Books"
+    },
+    {
+      _id: 106, title: "Boy's Fashion"
+    },
+    {
+      _id: 107, title: "Computers"
+    },
+    {
+      _id: 108, title: "Deals"
+    },
+    {
+      _id: 109, title: "Digital Music"
+    },
+    {
+      _id: 110, title: "Electronics"
+    },
+    {
+      _id: 111, title: "Girls Fashion"
+    },
+    {
+      _id: 112, title: "Health & houseHold"
+    },
+    {
+      _id: 113, title: "Home & kitchen"
+    },
+    {
+      _id: 114, title: "Industrial & Scientific"
+    },
+    {
+      _id: 115, title: "Kindle Store"
+    },
+    {
+      _id: 116, title: "Used Items"
+    },
+    {
+      _id: 117, title: "Misslenous"
+    },
+    {
+      _id: 118, title: "Raw Msterials"
+    },
+    {
+      _id: 119, title: "Music, CD & vinyl"
+    },
+    {
+      _id: 120, title: "Pet Suppliers"
+    },
+    {
+      _id: 121, title: "Prime Videos"
+    },
+    {
+      _id: 122, title: "Software"
+    },
+    {
+      _id: 123, title: "Sports & Outdoors"
+    },
+    {
+      _id: 124, title: "tool & home improvements"
+    },
+    {
+      _id: 125, title: "Toys & Games"
+    },
+    {
+      _id: 126, title: "Video Games"
+    },
+    {
+      _id: 127, title: "Women's Fashion"
+    }
+  ];
   return (
     <div>
       <div className='w-full bg-amazon_blue text-white px-4 py-3 flex items-center gap-4'>
@@ -22,21 +104,26 @@ const Header = () => {
         </div>
 
         <div className="h-10 rounded-md flex flex-grow relative">
-          <span  onClick={()=>setShowAll(!showAll)}
-          className="w-14 h-full bg-gray-200 hover:bg-gray-300 border-2 cursor-pointer duration-300 text-sm 
+          <span onClick={() => setShowAll(!showAll)}
+            className="w-14 h-full bg-gray-200 hover:bg-gray-300 border-2 cursor-pointer duration-300 text-sm 
           text-amazon_blue font-titleFont flex items-center justify-center rounded-tl-md rounded-bl-md">
             All <span></span><ArrowDropDownOutlinedIcon /></span>
-            {  showAll &&(
-                <div>
-                  <ul className="absolute w-56 h-80 top-10 left-0 overflow-y-scroll overflow-x-hidden
+          {showAll && (
+            <div>
+              <ul className="absolute w-56 h-80 top-10 left-0 overflow-y-scroll overflow-x-hidden
                    bg-white border-amazon_blue text-black p-2 flex-col gap-1 z-50">
-                    <li>All Deparments</li>
-                  </ul>
-                </div>
-            )}
-          <input type='text' className="h-full text-base text-amazon_blue flex flex-grow outline-none border-none px-2"/>
+                {
+                  allItems.map((item) => (
+                    <li key={item._id} className="text-sm tracking-wide font-titleFont border-b-[1px] border-b-transparent
+                    hover:border-b-amazon_blue cursor-pointer duration-200">{item.title}</li>
+                  ))
+                }
+              </ul>
+            </div>
+          )}
+          <input type='text' className="h-full text-base text-amazon_blue flex flex-grow outline-none border-none px-2" />
           <span className="w-12 h-full flex items-center justify-center bg-amazon_yellow
-           hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md"><SearchIcon/></span>
+           hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md"><SearchIcon /></span>
         </div>
 
 
