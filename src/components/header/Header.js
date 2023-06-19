0e5c4dcd43fded3ff496f82914735aa0b0f9c0ec
eslamyasser <e.yasser997@gmail.com>
2023-06-19 22:33:41 +0300

    handle the header & add Material Icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import {logo} from "../../assets/index"
 
 const Header = () => {
   return (
     <div className='w-full bg-amazon_blue text-white px-4 py-3'>
+
+
+      <div className="px-2 h-[80%] flex items-center border border-transparent hover:border-white cursor-pointer duration-100">
+        <img className="w-24 mt-2" src={logo} alt="logo"/>
+      </div>
+      <LocationOnOutlinedIcon></LocationOnOutlinedIcon>
+
         {}
         {}
         {}
