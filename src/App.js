1c4884c6be157c7f4a163c0b5789672a7f1eb63e
eslamyasser <e.yasser997@gmail.com>
2023-06-19 22:44:44 +0300

    Adds localization

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/header/Header";
 function App() {
   return (
-    <div>
+    <div className="font-bodyFont">
      <Header/>
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import {logo} from "../../assets/index"
+import { logo } from "../../assets/index"
 
 const Header = () => {
   return (
-    <div className='w-full bg-amazon_blue text-white px-4 py-3'>
-
-
-      <div className="px-2 h-[80%] flex items-center border border-transparent hover:border-white cursor-pointer duration-100">
-        <img className="w-24 mt-2" src={logo} alt="logo"/>
+    <div>
+      <div className='w-full bg-amazon_blue text-white px-4 py-3 flex items-center gap-4'>
+        <div className="px-2 h-[80%] flex items-center border border-transparent
+         hover:border-white cursor-pointer duration-100">
+          <img className="w-24 mt-2" src={logo} alt="logo" />
+        </div>
+        <div className="px-2 h-[80%] flex items-center border border-transparent
+         hover:border-white cursor-pointer duration-100">
+        <LocationOnOutlinedIcon />
+        <p className='text-sm '>Deliver to <span>Egypt</span></p>
       </div>
-      <LocationOnOutlinedIcon></LocationOnOutlinedIcon>
+      </div>
 
-        {}
-        {}
-        {}
+    
+
+
+      { }
+      { } 
+      { }
 
 
 
