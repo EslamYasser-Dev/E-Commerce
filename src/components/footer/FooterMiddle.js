96ac9376584e1a657be6b0f52f06bbfbe9ad42fe
eslamyasser <e.yasser997@gmail.com>
2023-06-21 17:27:04 +0300

    Nested Json Handling

--- src/components/footer/FooterMiddle.js
+++ src/components/footer/FooterMiddle.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import FooterMiddleList from './FooterMiddleList';
+import { middleList } from '../../constants';
 
 const FooterMiddle = () => {
+
     return (
         <div className='w-full bg-amazon_light text-white'>
             {/*------------------------------top stARTS HERE---------------------------------- */}
             <div className='w-full border-b-[1px] border-gray-500 py-10 '>
                 <div className=' max-w-5xl mx-auto text-gray-300'>
                     <div className='w-full grid grid-cols-4 place-items-center items-start'>
-                       <FooterMiddleList 
-                       title="Get to Know US"
-                       one="Careers"
-                       />
-                       <FooterMiddleList/>
-
-                       <FooterMiddleList/>
-
-                       <FooterMiddleList/>
-
+                         {
+                            middleList.map((item)=> (
+                                <FooterMiddleList 
+                                key={item._id} 
+                                title={item.title}
+                                items={item.listItem} 
+                                />
+                            ))
+                         }            
                     </div>
                 </div>
             </div>
--- src/components/footer/FooterMiddleList.js
+++ src/components/footer/FooterMiddleList.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const FooterMiddleList = ({title,one,two,three,four}) => {
+
+const FooterMiddleList = ({ title,items }) => {
     return (
         <div>
             <h3 className='font-titleFont text-white text-base font-semibold mb-3'>{title}</h3>
             <ul className='flex flex-col gap-2 font-bodyFont'>
-                <li className='footerlink'>{one}</li>
-                <li className='footerlink'>{two}</li>
-                <li className='footerlink'>{three}</li>
-                <li className='footerlink'>{four}</li>
-            
+               
+                {
+                    items.map((item)=> item.listData.map((data,i)=>(<li key ={i} className='footerlink'>{data}</li>)))
+                }
             </ul>
         </div>
     )
