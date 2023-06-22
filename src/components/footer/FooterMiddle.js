722c5dfff5c884c7db7a804f0fdcb470132c12bb
eslamyasser <e.yasser997@gmail.com>
2023-06-22 23:42:03 +0300

    bottom Footer

--- src/components/footer/FooterMiddle.js
+++ src/components/footer/FooterMiddle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import FooterMiddleList from './FooterMiddleList';
 import { middleList } from '../../constants';
+import {logo,egflag} from '../../assets/index'
 
 const FooterMiddle = () => {
 
@@ -12,7 +13,7 @@ const FooterMiddle = () => {
                     <div className='w-full grid grid-cols-4 place-items-center items-start'>
                          {
                             middleList.map((item)=> (
-                                <FooterMiddleList 
+                                <FooterMiddleList
                                 key={item._id} 
                                 title={item.title}
                                 items={item.listItem} 
@@ -23,6 +24,17 @@ const FooterMiddle = () => {
                 </div>
             </div>
             {/*------------------------------top ends HERE---------------------------------- */}
+            {/*------------------------------bottom start HERE---------------------------------- */}
+            <div className='w-full flex gap-6 item-center justify-center py-6' >
+                <div>
+                    <img className='w-20 pt-3' src={logo} alt='logo'/> 
+                </div>
+
+                <div className=''>
+                    <p>English</p>
+                </div>
+            </div>
+            {/*------------------------------bottom ends HERE---------------------------------- */}
         </div>
     )
 }
--- src/components/footer/FooterMiddleList.js
+++ src/components/footer/FooterMiddleList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 
-const FooterMiddleList = ({ title,items }) => {
+const FooterMiddleList = ({ title, items }) => {
     return (
         <div>
             <h3 className='font-titleFont text-white text-base font-semibold mb-3'>{title}</h3>
             <ul className='flex flex-col gap-2 font-bodyFont'>
-               
+
                 {
-                    items.map((item)=> item.listData.map((data,i)=>(<li key ={i} className='footerlink'>{data}</li>)))
+                    items.map((item) => item.listData.map((data, i) => (<li key={i} className='footerlink'>{data}</li>)))
                 }
             </ul>
         </div>
