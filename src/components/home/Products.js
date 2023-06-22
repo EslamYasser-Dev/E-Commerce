5ebe1389d1880a5b248e62b91f1c234d914a73d1
eslamyasser <e.yasser997@gmail.com>
2023-06-23 02:34:59 +0300

    fix the categoty to top right

--- src/components/home/Products.js
+++ src/components/home/Products.js
@@ -15,10 +15,12 @@ const Products = () => {
         <div className='max-w-screen-2xl mx-auto grid grid-cols-4 gap-10 px-4'>
             {
                 ProductData.map((item) => (
-                    <div className='bg-white h-auto border-[1px] border-gray-200 py-6 z-30 hover:border-transparent 
+                    <div className='bg-white h-auto border-[1px] border-gray-200 py-8 z-30 hover:border-transparent 
                     shadow-none hover:shadow-testShadow duration-200 flex flex-col gap-4 relative' key={item.id} >
+                        <span className='text-xs capitalize italic absolute top-2 right-2 text-gray-500'>{item.category}</span>
                         <div className='w-full h-auto flex items-center justify-center'>
                             <img className='w-52 h-64 object-contain ' src={item.image} alt='product' />
+                         
                         </div>
                         <div className='px-6'>
                             <div className='flex items-center justify-between'>
@@ -29,11 +31,14 @@ const Products = () => {
                             <div>
                                 <p className='text-sm'>{item.description.substring(0, 100)}...</p>
                                 <div className='text-yellow-500'>
+                                    {/* item.rating.rate */}
                                     <Star />
 
                                 </div>
                             </div>
-                            <button>Add to Cart</button>
+                            <button className='w-full font-titleFont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border
+                            hover:from-yellow-300 hover:to-yellow-400 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl
+                            active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3'>Add to Cart</button>
                         </div>
                     </div>
                 ))
